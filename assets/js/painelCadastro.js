5802b221e5c0c89da1430761683e8c4ec4d61305
nick <nick104@outlook.com>
2024-05-01 22:36:34 -0300

    Update painel de cadastro

--- assets/js/painelCadastro.js
+++ assets/js/painelCadastro.js
@@ -4,11 +4,15 @@ closeIcon = document.getElementById("close-modal");
 
 function abrirPainel(){
     modal.style.display = "block";
+    setTimeout(() => {
+        modal.style.transform = "translateX(0)";
+    }, 10);
     blurEffect.style.display = "inherit";
     document.body.style = "overflow-y: hidden;"
 }
 
 function fecharPainel(){
+    modal.style.transform = "translateX(100%)";
     modal.style.display = "none";
     blurEffect.style.display = "none";
     document.body.style = "overflow-y: auto;"
