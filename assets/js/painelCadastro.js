2a5e471502a03c86479778e48aabdb4ee284d591
nick <nick104@outlook.com>
2024-05-01 21:48:08 -0300

    Update files

--- assets/js/painelCadastro.js
+++ assets/js/painelCadastro.js
@@ -2,13 +2,13 @@ modal = document.getElementById("cadastro");
 blurEffect = document.getElementById("overlay");
 closeIcon = document.getElementById("close-modal");
 
-function openModal(){
+function abrirPainel(){
     modal.style.display = "block";
     blurEffect.style.display = "inherit";
     document.body.style = "overflow-y: hidden;"
 }
 
-function closeModal(){
+function fecharPainel(){
     modal.style.display = "none";
     blurEffect.style.display = "none";
     document.body.style = "overflow-y: auto;"
