308b951839a65718f2b8cf5016d414657f476275
nick <nick104@outlook.com>
2024-05-03 21:48:43 -0300

    Update home
    finalizando home e setando usernames via javascript

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,6 @@
 let enderecourl = 'https://go-wash-api.onrender.com/api/auth/address';
+let username_header = document.getElementById('username-header');
+let username_card = document.getElementById('username-card');
 async function endereco(){
     
     let tokenUser = JSON.parse(localStorage.getItem("user")).access_token;
@@ -14,4 +16,12 @@ async function endereco(){
     console.log(respostaApi)
 }
 
+function setUsername(){
+    let item = localStorage.getItem('user');
+    let user = JSON.parse(item)
+    username_header.textContent = `Olá ${user.user.name}!`
+    username_card.textContent = user.user.name
+}
+
+setUsername()
 endereco()
